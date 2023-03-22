5e6a16d63e623d5d0ba7a60f45c57d2ad6470a63
bvv94 <bvdevasconcelos@gmail.com>
2023-03-22 15:04:53 -0400

    Telas Login e Signin Funcionando OK

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -4,19 +4,42 @@ import InputStyled from "../Style/StyledInput"
 import ButtonStyled from "../Style/StyledButton"
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
+import apiAuth from "../services/apiAuth";
+import axios from "axios";
 
 export default function LogIn() {
+    // const [data, setData] = useState({ email: "", password: "" })
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    // const [user, setUser] = useContext()
+    const navigate = useNavigate();
 
+    function login(e) {
+
+        const body = {email, password}
+        console.log(body)
+        e.preventDefault();
+        const promise = axios.post("https://mock-api.driven.com.br/api/v4/driven-plus/auth/login", body)
+
+        promise.then((res) => {
+                console.log(res.data)
+                navigate('/subscriptions')
+            })
+        promise.catch((err) => {
+                alert(err.response.data.message)
+            })
+
+    }
 
     return (
         <>
-            <Form>
+            <Form onSubmit={login}>
                 <Logo src={driven} />
-                <InputStyled name="email" type="email" placeholder="E-mail" required />
-                <InputStyled name="password" type="password" placeholder="Senha" required />
-                <ButtonStyled>ENTRAR</ButtonStyled>
+                <InputStyled name="email" type="email" placeholder="E-mail" required 
+                    value={email} onChange={e=> setEmail(e.target.value)}/>
+                <InputStyled name="password" type="password" placeholder="Senha" required 
+                    value={password} onChange={e=> setPassword(e.target.value)}/>
+                <ButtonStyled type="submit">ENTRAR</ButtonStyled>
                 <Tosignin to="/cadastro">Não possui uma conta? Cadastre-se</Tosignin>
             </Form>
         </>)
--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -3,25 +3,43 @@ import InputStyled from "../Style/StyledInput"
 import ButtonStyled from "../Style/StyledButton"
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function SignIn() {
+
+    const [email, setEmail] = useState("")
     const [name, setName] = useState("")
     const [cpf, setCpf] = useState("")
-    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const navigate = useNavigate();
 
-    function login(){
+    function login(e) {
+
+        const body = { email, name, cpf, password }
+
+        e.preventDefault();
+        console.log("Cadastrando");
+
+        const promise = axios.post("https://mock-api.driven.com.br/api/v4/driven-plus/auth/sign-up", body)
+
         //se sucesso to=/subscriptions
+        promise.then((res) => {
+            console.log(res)
+            navigate("/subscriptions")
+        })
         //se falha to=/home e exibir alert
+        promise.catch((err) =>
+            alert(err.response.statusText)
+        )
     }
 
     return (
         <>
             <Form onSubmit={login}>
-                <InputStyled name="name" type="name" placeholder="Nome" required />
-                <InputStyled name="cpf" type="cpf" placeholder="CPF" required minLength="11"/>
-                <InputStyled name="email" type="email" placeholder="E-mail" required />
-                <InputStyled name="password" type="password" placeholder="Senha" required />
+                <InputStyled onChange={e=> setName(e.target.value)} value={name} name="name" type="text" placeholder="Nome" required />
+                <InputStyled onChange={e=> setCpf(e.target.value)} value={cpf} name="cpf" type="text" placeholder="CPF" required />
+                <InputStyled onChange={e=> setEmail(e.target.value)} value={email} name="email" type="email" placeholder="E-mail" required />
+                <InputStyled onChange={e=> setPassword(e.target.value)} value={password} name="password" type="password" placeholder="Senha" required />
                 <ButtonStyled type="submit">ENTRAR</ButtonStyled>
                 <Tosignin to="/">Já possui uma conta? Entre</Tosignin>
             </Form>
